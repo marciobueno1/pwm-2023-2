75deaf5c7f73d5b2d1a385bde65875b33b1dbc5f
Marcio Bueno <marciobueno@gmail.com>
2023-10-24 10:37:06 +0000

    Android 01 - cartão de aniversário (texto)

---
+++ android01-happyBirthday/HappyBirthday/app/src/main/java/br/unicap/c3/pwm/happybirthday/MainActivity.kt
@@ -0,0 +1,63 @@
+package br.unicap.c3.pwm.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.unicap.c3.pwm.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                ) {
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "Minha Visualização")
+@Composable
+fun DefaultPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+    }
+}
